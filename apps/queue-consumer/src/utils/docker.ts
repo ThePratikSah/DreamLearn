3f84fd85edb5947dd6251a313f74692ac9c08b98
Pratik Sah <shivam3448@gmail.com>
2024-08-12 01:00:24 +0530

    Added docker util to run locally

---
+++ apps/queue-consumer/src/utils/docker.ts
@@ -0,0 +1,23 @@
+import Docker from "dockerode";
+import {
+  awsAccessKey,
+  awsSecretAccessKey,
+  prodBucketName,
+  tempBucketName,
+} from "../config";
+
+export async function startDockerLocally(key: string) {
+  const docker = new Docker();
+  const container = await docker.createContainer({
+    Image: "video-transcoder",
+    Env: [
+      "KEY=" + key,
+      "AWS_TEMP_BUCKET=" + tempBucketName,
+      "AWS_PROD_BUCKET=" + prodBucketName,
+      "AWS_ACCESS_KEY=" + awsAccessKey,
+      "AWS_SECRET_ACCESS_KEY=" + awsSecretAccessKey,
+    ],
+  });
+
+  await container.start();
+}
