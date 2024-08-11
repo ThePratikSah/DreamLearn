ca8c4b05cd038f64420cfc4a11c2c716eb0427a6
Pratik Sah <shivam3448@gmail.com>
2024-08-11 18:50:12 +0530

    Added sqs handler logic

---
+++ apps/queue-consumer/src/sqshandler.ts
@@ -0,0 +1,13 @@
+import { Message } from "@aws-sdk/client-sqs";
+import type { S3Event } from "aws-lambda";
+
+export async function sqsHandler(message: Message) {
+  const { MessageId, Body } = message;
+
+  if (!Body) {
+    return console.log("no body");
+  }
+  const s3Event: S3Event = JSON.parse(Body);
+
+  console.log({ s3Event });
+}
