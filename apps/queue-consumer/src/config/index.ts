76cdbadfeb60d7c637b3338dd0274f305fd92c35
Pratik Sah <shivam3448@gmail.com>
2024-08-12 00:24:00 +0530

    Added logic for starting a new container on file upload

--- apps/queue-consumer/src/config/index.ts
+++ apps/queue-consumer/src/config/index.ts
@@ -1,5 +1,15 @@
 export const tempQueueUrl = process.env.AWS_TEMP_QUEUE_URL || "";
 export const tempBucketName = process.env.AWS_TEMP_BUCKET || "";
+export const prodBucketName = process.env.AWS_PROD_BUCKET || "";
 export const awsAccessKey = process.env.AWS_ACCESS_KEY || "";
 export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || "";
 export const awsRegion = process.env.AWS_REGION || "ap-south-1";
+export const awsTaskDef = process.env.AWS_TASK_DEF || "";
+export const awsEcsCluster = process.env.AWS_ECS_CLUSTER || "";
+export const awsConfig = {
+  region: awsRegion,
+  credentials: {
+    accessKeyId: awsAccessKey,
+    secretAccessKey: awsSecretAccessKey,
+  },
+};
--- apps/queue-consumer/src/sqshandler.ts
+++ apps/queue-consumer/src/sqshandler.ts
@@ -1,5 +1,9 @@
 import { Message } from "@aws-sdk/client-sqs";
+import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
 import type { S3Event } from "aws-lambda";
+import { awsConfig, awsEcsCluster, awsTaskDef } from "./config";
+
+const ecsClient = new ECSClient(awsConfig);
 
 export async function sqsHandler(message: Message) {
   try {
@@ -9,8 +13,42 @@ export async function sqsHandler(message: Message) {
       return console.log("no body");
     }
     const s3Event: S3Event = JSON.parse(Body);
+    if ("Service" in s3Event && "Event" in s3Event) {
+      if (s3Event.Event === "s3:TestEvent") {
+        return;
+      }
+    }
 
-    console.log({ s3Event });
+    for (const record of s3Event.Records) {
+      const { s3 } = record;
+      const {
+        object: { key },
+      } = s3;
+
+      console.log("Running new task on ECS...");
+      const taskRun = new RunTaskCommand({
+        taskDefinition: awsTaskDef,
+        cluster: awsEcsCluster,
+        launchType: "FARGATE",
+        networkConfiguration: {
+          awsvpcConfiguration: {
+            securityGroups: ["sg-478b0522"],
+            assignPublicIp: "ENABLED",
+            subnets: ["subnet-2aab1251", "subnet-f14e39bd", "subnet-259b9d4d"],
+          },
+        },
+        overrides: {
+          containerOverrides: [
+            {
+              name: "video-transcoder",
+              environment: [{ name: "KEY", value: key }],
+            },
+          ],
+        },
+      });
+
+      await ecsClient.send(taskRun);
+    }
   } catch (error) {
     console.error(error);
   }
