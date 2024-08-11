1589ff5e991f1aec015058d6b7f9a583edc6de48
Pratik Sah <shivam3448@gmail.com>
2024-08-11 18:56:15 +0530

    Reformated entire codebase

--- apps/queue-consumer/src/sqshandler.ts
+++ apps/queue-consumer/src/sqshandler.ts
@@ -1,12 +1,12 @@
-import { Message } from '@aws-sdk/client-sqs';
-import type { S3Event } from 'aws-lambda';
+import { Message } from "@aws-sdk/client-sqs";
+import type { S3Event } from "aws-lambda";
 
 export async function sqsHandler(message: Message) {
   try {
     const { MessageId, Body } = message;
 
     if (!Body) {
-      return console.log('no body');
+      return console.log("no body");
     }
     const s3Event: S3Event = JSON.parse(Body);
 
